bbb8731e07efbc1766d01d7f16a9172607d624a1
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-02-01 18:26:01 +0100

    mod

---
+++ es4/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace es4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dammi una frase");
+            string frase = Console.ReadLine();
+            string[] parole = frase.Split(" ");
+            for (int i = parole.Length - 1; i >= 0; i--)
+            {
+
+                Console.Write(parole[i]);
+                Console.Write("  ");
+
+            }
+        }
+    }
+}
