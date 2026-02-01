2419a373c8fede600895552fdd533de2549e21cd
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-02-01 18:20:22 +0100

    mod

---
+++ es2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace es2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci una stringa");
+            string s = Console.ReadLine();
+            char[] stringhe = s.ToCharArray();
+            double metà = stringhe.Length / 2;
+            for (int i = 0;i < metà; i++)
+            {
+                Console.Write(stringhe[i]);
+
+            }
+            Console.WriteLine("--------------");
+            for (double i = metà; i < stringhe.Length; i++)
+            {
+                
+                Console.Write(stringhe[(int)i]);
+            }
+
+        }
+    }
+}
