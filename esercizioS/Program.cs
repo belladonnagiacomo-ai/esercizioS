ac4010890df9a6f3eda877e4541680670c5be6e3
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-02-01 17:27:16 +0100

    mod

--- esercizioS/Program.cs
+++ esercizioS/Program.cs
@@ -8,14 +8,21 @@ namespace esercizioS
         {
             Console.WriteLine("Inserisci un numero binario lungo 8 bit");
             string nome = Console.ReadLine();
-            double somma = 0;
+            double somma = 0, risultato = 0;
             char[] decimale = nome.ToCharArray();
             for(int i = 0; i < decimale.Length; i++)
             {
 
-                double risultato = Math.Pow(2,i);
-                Console.WriteLine(somma);
-                somma = somma + risultato;
+                if (decimale[i] == '0')
+                {
+                    Console.WriteLine("zero");
+                }
+                else {
+                    risultato = Math.Pow(2, i);
+                    somma = somma + risultato;
+                }
+
+                    
                 
             }
             Console.WriteLine("Il numero convertito e: " + somma);
