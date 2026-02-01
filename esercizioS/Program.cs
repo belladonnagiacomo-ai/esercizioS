b345a35c48076cf82eee537181f1b28081e2c12c
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-02-01 16:42:49 +0100

    Aggiungere i file di progetto.

---
+++ esercizioS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace esercizioS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
