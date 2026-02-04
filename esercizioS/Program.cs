49e5e14a18670051b1e47136a629b7a059635f93
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-02-04 12:41:22 +0100

    mod

--- esercizioS/Program.cs
+++ esercizioS/Program.cs
@@ -10,15 +10,15 @@ namespace esercizioS
             string nome = Console.ReadLine();
             double somma = 0, risultato = 0;
             char[] decimale = nome.ToCharArray();
-            for(int i = 0; i < decimale.Length; i++)
+            for(int i = 1; i <= decimale.Length; i++)
             {
 
-                if (decimale[i] == '0')
+                if (decimale[i - 1] == '0')
                 {
                     Console.WriteLine("zero");
                 }
                 else {
-                    risultato = Math.Pow(2, i);
+                    risultato = Math.Pow(2, decimale.Length - i);
                     somma = somma + risultato;
                 }
 
